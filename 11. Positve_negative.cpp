1c92c571de9f21be8503bafbbb461f64e937714a
Binoy Das Gupta <62295509+Binoy02@users.noreply.github.com>
2021-07-06 12:25:56 +0600

    Add files via upload

---
+++ 11. Positve_negative.cpp
@@ -0,0 +1,23 @@
+/*
+  Binoy Das Gupta
+  Check if a number is positive or negative
+  06-07-2021
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Enter a number : ";
+	cin>>x;
+	if(x>0)
+	{
+		cout<<""<<x<<" is positive"<<endl;
+	}
+	else 
+	{
+		cout<<""<<x<<" is negative"<<endl;
+	}
+	return 0;
+}
