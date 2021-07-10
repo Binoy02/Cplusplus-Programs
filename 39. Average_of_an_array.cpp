e68e0abe4967fdd1830bea9cc1e5635d142650ec
Binoy Das Gupta <62295509+Binoy02@users.noreply.github.com>
2021-07-10 17:33:26 +0600

    Add files via upload

---
+++ 39. Average_of_an_array.cpp
@@ -0,0 +1,28 @@
+/*
+  Binoy Das Gupta
+  Calculate average of elements in an array.
+  10-07-2021
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int A[100];
+	int size,i,sum;
+	cout<<"Enter array size : ";
+	cin>>size;
+	cout<<"Enter array elements : ";
+	for(i=0;i<size;i++)
+	{
+		cin>>A[i];
+	}
+	sum=0.0;
+	for(i=0;i<size;i++)
+	{
+		sum = sum + A[i];
+	}
+	float average = (float)sum/size;
+	cout<<"Average of the array is = "<<average<<endl;
+	return 0;
+}
