d847f1db51e5f698d9f1f9f59d7ce88b8e6fc64b
Binoy Das Gupta <62295509+Binoy02@users.noreply.github.com>
2021-07-11 20:02:07 +0600

    Add files via upload

---
+++ 41. Pointer_introduction.cpp
@@ -0,0 +1,20 @@
+/*
+  Binoy Das Gupta
+  Pointer_Introduction
+  11-07-2021
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=10;
+	int *p;
+	p=&x;
+	cout<<x<<endl;
+	cout<<&x<<endl;
+	cout<<p<<endl;
+	cout<<&p<<endl;
+	cout<<*p<<endl;
+	return 0;
+}
