029439039065361e61c708b331c7b994f422cbcd
Binoy Das Gupta <62295509+Binoy02@users.noreply.github.com>
2021-07-14 12:10:57 +0600

    Add files via upload

---
+++ 44. String(1).cpp
@@ -0,0 +1,20 @@
+/* 
+  Binoy Das Gupta
+  
+  Reading and printing string
+  1.Taking a single name and printing it
+  
+  14-07-2021
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	char S1[100];
+	cout<<"Enter your name : ";
+	cin>>S1;
+	cout<<"Welcome "<<S1<<endl;
+	
+	return 0;
+}
