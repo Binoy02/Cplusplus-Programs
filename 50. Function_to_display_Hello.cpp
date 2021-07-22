4127e494cc39aa4464ee20fcc46c9afaa459fa9c
Binoy Das Gupta <62295509+Binoy02@users.noreply.github.com>
2021-07-22 10:18:54 +0600

    Add files via upload

---
+++ 50. Function_to_display_Hello.cpp
@@ -0,0 +1,20 @@
+/*
+  Binoy Das Gupta
+  Function to display a "Hello" message 
+  22-07-2021
+*/
+
+#include<iostream>
+using namespace std;
+
+void display()
+{
+	cout<<"Hello"<<endl;
+}
+
+int main()
+{
+	display();
+	return 0;
+}
+
